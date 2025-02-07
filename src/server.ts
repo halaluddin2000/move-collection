c861185e229aabb67a558fd235f6842cb81ad103
halaluddin2000 <helaluddin7001@gmail.com>
2025-02-08 01:23:42 +0600

    mongoose added

--- src/server.ts
+++ src/server.ts
@@ -4,8 +4,8 @@ import config from "./config";
 
 async function main() {
   try {
-    await mongoose.connect(config.port as string);
-    const port = 5000;
+    await mongoose.connect(config.db_url as string);
+    // const port = 5000;
     app.listen(config.port, () => {
       console.log(`Example app listening on port ${config.port}`);
     });
