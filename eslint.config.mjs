7d315ee93e5c0526299d94479d6104a34bd87147
halaluddin2000 <helaluddin7001@gmail.com>
2025-02-08 17:47:42 +0600

    eslit or typeScript added

---
+++ eslint.config.mjs
@@ -0,0 +1,23 @@
+import globals from "globals";
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+    eslint.configs.recommended,
+    tseslint.configs.recommended,
+    {
+        languageOptions: {
+            globals: {
+                ...globals.node,
+            }
+        }
+    },
+    {
+        rules: {
+            "no-unused-vars": "warn",
+            "no-undef": "warn",
+            "prefer-const": "error",
+            "no-console": "warn"
+        }
+    }
+);
--- src/server.ts
+++ src/server.ts
@@ -5,9 +5,9 @@ import config from "./config";
 async function main() {
   try {
     await mongoose.connect(config.db_url as string);
-    // const port = 5000;
-    app.listen(config.port, () => {
-      console.log(`Example app listening on port ${config.port}`);
+    const port = 5000;
+    app.listen(port, () => {
+      console.log(`Example app listening on port ${port}`);
     });
   } catch (err) {
     console.log(err);
